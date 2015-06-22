76a49df0dcbf9afc283103463d9498313ef65495
huangshan108 <shanhuang108@gmail.com>
2015-06-22 16:42:35 -0700

    users added

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,7 +9,18 @@ class SessionController < ApplicationController
 	end
 
 	def show
+		if Session.find_by_id(params[:id]) == nil
+			flash[:error] = "Session not found"
+			redirect_to root_path
+			return
+		end
 		if params[:name]
+			s = Session.find(params[:id])
+			if s.users.all.map(&:name).include? params[:name]
+				# do some logic when user is found
+			else
+				u = User.create(:name => params[:name], :session_id => params[:id])
+			end
 			render 'show'
 		else
 			render 'join'
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622221148) do
+ActiveRecord::Schema.define(version: 20150622231913) do
 
   create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
