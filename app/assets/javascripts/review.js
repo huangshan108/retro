65f2866fb917c18be61be049ea8cd7fee6a17d36
huangshan108 <shanhuang108@gmail.com>
2015-06-23 16:53:06 -0700

    support submitting thumb votes via keypress

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -9,19 +9,35 @@ $(function() {
 			thumb_vote_url = '/issues/thumb-down';
 			issue_id = $(e.currentTarget).data('issue-id');
 		}
-		$.ajax({
-			url: thumb_vote_url,
-			data: {
-				'issue_id': issue_id, 
-			},
-			type: 'POST',
-			dataType: 'JSON',
-			success: function() {
+		do_thumb_vote_ajax(thumb_vote_url, issue_id);
+	});
 
-			},
-			error: function() {
-				alert("Error when thumb vote.")
-			}
-		});
+	$('body').keypress(function(e) {
+		e.preventDefault();
+		issue_id = $('.thumb-wrapper.up').data('issue-id');
+		// enter
+		if (e.which == 13) { 
+			do_thumb_vote_ajax('/issues/thumb-up', issue_id);
+		// space 
+		} else if (e.which == 32) {
+			do_thumb_vote_ajax('/issues/thumb-down', issue_id);
+		};
 	});
 });
+
+function do_thumb_vote_ajax (thumb_vote_url, issue_id) {
+	$.ajax({
+		url: thumb_vote_url,
+		data: {
+			'issue_id': issue_id, 
+		},
+		type: 'POST',
+		dataType: 'JSON',
+		success: function() {
+
+		},
+		error: function() {
+			alert("Error when thumb vote.")
+		}
+	});
+}
