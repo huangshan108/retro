2450b6a7c9f825b62c7dd29c5ba7999821ecd8ac
huangshan108 <shanhuang108@gmail.com>
2015-06-23 03:05:51 -0700

    notes now added via ajax

---
+++ app/assets/javascripts/notes.js
@@ -0,0 +1,18 @@
+$(function() {
+	console.log("nima");
+	$('#submit-note').click(function() {
+		note = $('.new-note input').val();
+		$.ajax({
+			url: '/issues/new-note',
+			type: 'POST',
+			dataType: 'JSON',
+			data: {'note': note, 'issue_id': $('.new-note input').data('issue-id')},
+			success: function(response) {
+				
+			},
+			error: function(response) {
+				alert("Error when posting notes");
+			}
+		});
+	});
+});
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,9 @@
 class NotesController < ApplicationController
 	def create
 		Note.create(:detail => params[:note], :issue_id => params[:issue_id])
-		redirect_to :back
+		respond_to do |format|
+			format.html { }
+			format.json { render :json => {:data => "succeed!" } }
+		end
 	end
 end
