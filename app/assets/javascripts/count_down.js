ea539e9e51503114f4c09cd3a1051c3dc919f922
huangshan108 <shanhuang108@gmail.com>
2015-07-06 00:36:09 -0700

    thumb vote count down added

--- app/assets/javascripts/count_down.js
+++ app/assets/javascripts/count_down.js
@@ -1,8 +1,12 @@
 function count_down(init) {
-  $('#sec-left').text(init);
+  var $sec = $('#sec-left');
+  $sec.text(init);
   var inter = setInterval(function () {
     init -= 1;
-    $('#sec-left').text(init);
+    $sec.text(init);
+    if (init == 10) {
+      $sec.removeClass('green').addClass('red');
+    };
     if (init == 0) {
       console.log('cleared');
       clearInterval(inter);
