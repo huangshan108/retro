16caf3edd31f35031b09a1f17313793b47f3ac59
huangshan108 <shanhuang108@gmail.com>
2015-07-08 00:37:43 -0700

    use max thread 256 for free dyno

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 1)
-threads_count = Integer(ENV['MAX_THREADS'] || 16)
+threads_count = Integer(ENV['MAX_THREADS'] || 256)
 threads threads_count, threads_count
 
 preload_app!
