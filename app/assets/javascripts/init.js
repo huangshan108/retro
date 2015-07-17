b43f2c5318f1c9bce41b7e56d380b0cd29c2f2ca
huangshan108 <shanhuang108@gmail.com>
2015-07-17 00:21:44 -0700

    footer added

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,4 +1,5 @@
 $(function() {
+  resizeWindow();
   var issue_socket = new IssueSocket();
 
   if ($('.thumb-graph-wrapper').length > 0) {
@@ -8,3 +9,15 @@ $(function() {
     issue_socket.thumbVoteCallback(obj);
   };
 });
+
+$(window).resize(function() {
+  resizeWindow();
+});
+
+function resizeWindow() {
+  var $app_container = $('.app-container');
+  var $window = $(window);
+  if ($app_container.height() < $window.height()) {
+    $app_container.height($window.height() - 150);
+  };
+};
